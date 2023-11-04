64f34d26935558f2e1506d156379aa5d7d78a779
Mikhail <Popov.Mikhail.A@gmail.com>
2023-11-05 00:16:01 +0600

    Семинар 01

---
+++ SW_01/art.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, char **argv) {
+/*
+(\___/)
+(='.'=)
+(")_(")
+*/
+    // printf("(\\___/)\n");
+    // printf("(='.'=)\n");
+    // printf("(\")_(\")\n");
+    // return 0;
+
+    printf("  |\\_/|\n");
+   // printf("\n");
+    printf(" / @ @ \\\n");
+   // printf("\n");
+    printf("( > * < )\n");
+   // printf("\n");
+    printf("  >>x<<\n");
+   // printf("\n");
+    printf("  / o \\\n");
+    return 0;
+
+}
